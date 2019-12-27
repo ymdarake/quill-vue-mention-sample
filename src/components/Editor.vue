45b6de37b85b53141c15687ce8dbfd50c3d59c46
ymdarake <t.sakamoto.ouen@gmail.com>
2019-12-27 14:08:29 +1300

    [add]toolbar options (like bold, talic, code-block)

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -18,6 +18,24 @@ import { quillEditor } from 'vue-quill-editor'
 import 'quill-mention';
 import 'quill-mention/dist/quill.mention.min.css';
 
+// @link https://quilljs.com/docs/modules/toolbar/
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
 
 export default {
   name: 'HelloWorld',
@@ -29,6 +47,7 @@ export default {
       content: '',
       editorOption: {
         modules: {
+          toolbar: toolbarOptions,
           mention: {
             allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
             mentionDenotationChars: ["@", "#"],
